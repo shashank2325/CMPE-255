e9a2d6a231bd43fb788f15091a89d293ea223212
Shashank Muthuraj <shashankm2325@gmail.com>
2023-10-12 13:03:02 -0700

    change1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.write("Hello World")
+
+dataset_url = 'https://raw.githubusercontent.com/shashank2325/CMPE-255/main/Bike_Share_SFO_2017.csv'
+df = pd.read_csv(dataset_url).drop(columns=['Unnamed: 0'])
+
+
+df['hour'] = pd.to_datetime(df['start_date']).dt.hour
+
+fig1 = px.histogram(df, x='hour', title='Bike Usage Throughout the Day',
+                    labels={'hour': 'Hour of the Day'}, nbins=24)
+fig1.update_xaxes(range=[0, 23], dtick=1)
+
+st.plotly_chart(fig1)
+
